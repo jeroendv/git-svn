e8fb9fb7562a2d61e32b00b3334e81f26480d5c6
jeroen_devlieger <jeroen.devlieger@nikon.com>
2018-08-01 14:34:05 +0200

    function decorator to easily time function calls

--- git_svn/__init__.py
+++ git_svn/__init__.py
@@ -5,3 +5,20 @@ try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
     __version__ = 'unknown'
+
+from functools import wraps
+from time import time
+
+def timeit(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func:{} args:[{}, {}] took: {:2.4f} sec'.format(
+            f.__name__
+            , args
+            , kw
+            , te-ts))
+        return result
+    return wrap
