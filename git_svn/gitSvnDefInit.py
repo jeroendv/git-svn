7a3801accf95501fc603cff0366e7bf227185e64
jeroen_devlieger <jeroen.devlieger@nikon.com>
2018-11-05 00:50:44 +0100

    refactor and make some more objects

--- git_svn/gitSvnDefInit.py
+++ git_svn/gitSvnDefInit.py
@@ -60,9 +60,9 @@ def main():
     git_svn_init(git_svn_def.url)
 
     # add config key and git branch ref for each branch
-    for branchpath in git_svn_def.branches:
-        add_git_svn_branch_configuration(branchpath)
-        add_git_svn_branch_reference(branchpath)
+    for branch in git_svn_def.branches:
+        add_git_svn_branch_configuration(branch)
+        add_git_svn_branch_reference(branch)
 
     # trick git-svn into rebuilding the rev_map by querying the svn info
     subprocess.call(['git', 'svn', 'info'])
@@ -100,64 +100,57 @@ def git_svn_init(url):
     assert currentUrl == url
     print("git-svn bridge is already initalized for: " + url, flush=True)
 
-def add_git_svn_branch_configuration(svn_branch_path):
+def add_git_svn_branch_configuration(branch: GitSvnBranchDef):
     """idenpotent git-svn branch configuration """
-    branch_name = os.path.basename(svn_branch_path)
+    branch_set = GitSvnConfigBranchSet()
 
-    git_ref = "refs/remotes/git-svn/{}".format(branch_name)
-
-    svn_git_fetchMap = GitSvnConfigFetchDef()
-
-    if svn_branch_path in svn_git_fetchMap and git_ref == svn_git_fetchMap[svn_branch_path]:
+    if branch in branch_set:
         # nothign to do this config key already exists
         print("svn-remote.svn.fetch config key already exists for: " + svn_branch_path, flush=True)
         return 
     
-    if branch_name in svn_git_fetchMap:
-        assert git_ref != svn_git_fetchMap[svn_branch_path]
-        raise Exception("""git branch is already tracked by: {}
-Can't add second tracking branch reference: {}""".format(svn_git_fetchMap[svn_branch_path], git_ref))
+    b = branch_set.get_branch_for_svn_path(branch.svn_path)
+    if b is not None:
+        assert branch.git_svn_ref != b.git_svn_ref
+        raise Exception("""svn path: {}
+is already tracked by: {}
+Can't add second tracking branch reference: {}""".format(branch.svn_path, b.git_svn_ref, branch.git_svn_ref))
     
-    if git_ref in svn_git_fetchMap.values():
-        existing_svn_path = None
-        for (k,v) in svn_git_fetchMap.items():
-            if v == git_ref:
-                existing_svn_path = k
-                break
-        raise Exception("""git branch refernce is already tracking: {existing_svn_path}
-the same branch reference can't track a second svn path: {}""".format("", svn_branch_path))
+    b = branch_set.get_branch_for_git_svn_ref(branch.git_svn_ref)
+    if b is not None:
+        assert branch.svn_path != b.svn_path
+        raise Exception("""git branch: {}
+is already tracking svn path : {}
+the same branch reference can't track a second svn path: {}""".format(b.git_svn_ref, b.svn_path, branch.svn_path))
 
 
     # add the svn-remote.svn.fetch config key as it does not yet exists
-    print("adding svn-remote.svn.fetch config key to track: " + svn_branch_path, flush=True)
+    print("adding svn-remote.svn.fetch config key to track: " + branch.svn_path, flush=True)
     cli_cmd = ['git',  "config", "--local",
-        "--add", "svn-remote.svn.fetch",  "%s:refs/remotes/git-svn/%s" % (svn_branch_path, branch_name)]
+        "--add", "svn-remote.svn.fetch",  branch.unparse()]
     subprocess.check_output(cli_cmd)
 
-def add_git_svn_branch_reference(svn_branch_path, remote = "origin"):
+def add_git_svn_branch_reference(branch: GitSvnBranchDef, remote = "origin"):
     """idempotent git-svn branch reference creation"""
     branch_name = os.path.basename(svn_branch_path)
 
-    svn_ref = "refs/remotes/git-svn/{}".format(branch_name)
-    git_ref = "refs/remotes/{}/{}".format(remote, branch_name)
-
-    if branch_exists(svn_ref):
+    if branch_exists(branch.git_svn_ref):
         # the branch reference alreasy exists, nothing to do :-)
         print("git-svn branch reference already exists for: " + svn_branch_path, flush=True)
 
         return 
 
     # check if the git_ref exists
-    if not branch_exists(git_ref):
+    if not branch_exists(branch.git_remote_ref):
         msg = """Failed to add git-svn tracking branch reference: {}
 Source branch reference does not exists: {}
 """
-        msg = msg.format(svn_ref, git_ref)
+        msg = msg.format(branch.git_svn_ref, branch.git_remote_ref)
         raise Exception(msg)
 
     # create the svn branch ref based on the git branch ref
     print("Adding git-svn branch reference for: " + svn_branch_path, flush=True)
-    cli_cmd = ['git', 'update-ref', svn_ref, git_ref]
+    cli_cmd = ['git', 'update-ref', branch.git_svn_ref, branch.git_remote_ref]
     output = subprocess.check_output(cli_cmd).decode()
     DebugLog.print(output)
 
@@ -182,20 +175,21 @@ def GitSvnUrl():
         assert e.returncode == 1 # a return code other then 1 means there is a bug!
         return None
 
-def GitSvnConfigFetchDef():
+def GitSvnConfigBranchSet():
     try:
         output = subprocess.check_output(['git', 'config', '--local', '--get-all', 'svn-remote.svn.fetch']).decode()
 
-        git_svn_map = {}
+        branch_set = BranchSet()
         for line in output.splitlines():
-            [svn_path, git_ref] = line.split(':')
-            git_svn_map[svn_path] = git_ref
+            branch_set.append(Branch(line))
 
-        return git_svn_map
+        return branch_set
 
     except subprocess.CalledProcessError as e:
         assert e.returncode == 1 # a return code other then 1 means there is a bug!
-        return dict()
+        return BranchSet()
+        
+
 
 class GitSvnDef(object):
     @property
@@ -203,11 +197,12 @@ class GitSvnDef(object):
         return self._url
     @property
     def branches(self):
-        return self._branches
+        return self._branch_set
 
-    def __init__(self, url, branches):
+    def __init__(self, url):
         self._url = url
-        self._branches = set(branches)
+        self._branch_set = BranchSet()
+
 
     @classmethod
     def parseConfig(cls, config:str):
@@ -230,18 +225,102 @@ class GitSvnDef(object):
                 raise Exception("Each of the branches key list items should be a url string.  but got" + str(yamlConfig['url']))
 
         # 
-        return GitSvnDef(yamlConfig['url'], yamlConfig['branches'])
+        git_svn_def = GitSvnDef(yamlConfig['url'])
+        for b in yamlConfig['branches']:
+            git_svn_def.branches.append(GitSvnBranchDef(b))
+
+        return git_svn_def
 
 
+
+class Branch(Object):
+    """git config svn-remote.svn.fetch value"""
+    
+    @property
+    def svn_path(self):
+        """svn repo path to the branch"""
+        return self._svn_path
+    
+    @property
+    def branch_name(self):
+        """name of the branch (same for git and svn)"""
+        return os.path.basename(self._svn_path)
+    
+    @property
+    def git_svn_ref(self):
+        """the git-svn remote tracking branch reference
+        
+        This is the git remote tracking branch that tracks the svn branch, 
+        I.e. it is the branch managed by the git-svn bridge """
+        return self._git_svn_ref
+
+    def __init__(self, svn_path, git_svn_ref):
+        self._svn_path = svn_path
+        self._git_svn_ref
+
+    @classmethod
+    def parse(cls, git_config_str):
+        [svn_path, git_svn_ref] = git_config_str.split(':')
+        return Branch(svn_path, git_svn_ref)
+
+    def unparse(self):
+        return self.svn_path + ":" + self.git_svn_ref
+
+class BranchSet(Object):
+    """a git-svn bridge can track multiple svn branches at once"""
+
+    @property
+    def __iter__(self):
+        return self._branches
+
+    def __init__self(self):
+        self._branches = []
+
+    def get_branch_for_svn_path(self, svn_path):
+        for b in self._branches:
+            if b.svn_path == svn_path:
+                return b
+        
+        return None
+
+    def get_branch_for_git_svn_ref(self, git_svn_ref):
+        for b in self._branches:
+            if b.git_svn_ref == git_svn_ref:
+                return b
+
+        return None
+    
+    def append(self, branch:Branch):
+        if self.get_branch_for_svn_path(branch.svn_path) is not None:
+            raise Exception("svn_path '{}' is already being tracked".format(branch.svn_path))
+        
+        if self.get_branch_for_git_svn_ref(branch.git_svn_ref) is not None:
+            raise Exception("git_svn_ref '{}' is already being tracking an svn path".format(branch.git_svn_ref))
+        
+        self._branches.append(branch)
+
+    def __in__ (self, branch:Branch):
+        b = self.get_branch_for_svn_path(branch.svn_path)
+        if b is None:
+            return False
+        else:
+            return b.git_svn_ref == branch.git_svn_ref
+
+
+class GitSvnBranchDef(Branch):
+    def git_remote_ref(self, remote=origin):
+        """the reference of this branch as tracked by another git repo.
+
+        I.e. it is git_svn_ref branch duplicated on a global git repo"""
+
+        return "refs/remotes/{}/{}".format(remote, self.branch_name)
+
+    def __init__(self, svn_path):
+        super(svn_path, self.__git_svn_ref(svn_path))
+
+    def __git_svn_ref(self, svn_path):
+        branch_name = os.path.basename(svn_path)
+        return "refs/remotes/git-svn/{}".format(branch_name)
     
 
 
-
-
-    
-    
-
-    
-
-
-    
