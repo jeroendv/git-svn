eae2be5fc8d448501fc54b6ad7aaa9ecd65b8475
jeroen_devlieger <jeroen.devlieger@nikon.com>
2018-11-06 23:04:37 +0100

    also parse and set the svn-remote.svn.ignore-paths config key

--- git_svn/gitSvnDefInit.py
+++ git_svn/gitSvnDefInit.py
@@ -64,6 +64,9 @@ def main():
         add_git_svn_branch_configuration(branchpath)
         add_git_svn_branch_reference(branchpath)
 
+    # add the ignore-paths config key
+    add_git_svn_ignore_paths(git_svn_def.ignore_paths)
+
     # trick git-svn into rebuilding the rev_map by querying the svn info
     subprocess.call(['git', 'svn', 'info'])
 
@@ -161,6 +164,22 @@ Source branch reference does not exists: {}
     output = subprocess.check_output(cli_cmd).decode()
     DebugLog.print(output)
 
+def add_git_svn_ignore_paths(ignore_paths:list):
+    """add the ignore_paths to the git-svn bridge config
+    """
+
+    # fail if the config key is already set
+    cli = ["git", "config", "--local", "--get", "svn-remote.svn.ignore-paths"]
+    rc = subprocess.check_call(cli)
+    if rc != 0:
+        raise Exception("the git-svn bridge already has a svn-remote.svn.ignore-paths config key")
+
+    # set the config key
+    configStr = "(" + "|".join(ignore_paths) + ")"
+    cli = ["git", "config", "--local", 
+        "svn-remote.svn.ignore-paths", configStr]
+    subprocess.check_output(cli)
+
 
 def branch_exists(git_ref):
     """check if a certain branch exists"""
@@ -200,14 +219,35 @@ def GitSvnConfigFetchDef():
 class GitSvnDef(object):
     @property
     def url(self):
+        """the url of the svn repository
+        
+        SA git config key: svn-remote.svn.url
+        """
         return self._url
     @property
     def branches(self):
+        """the set of branches to be tracked
+
+        a branch is identified by its path in the repository
+        SA git config key svn-remote.svn.fetch
+        """
         return self._branches
 
-    def __init__(self, url, branches):
+    @property
+    def ignore_paths(self):
+        """the set of repository paths that are to be ignored
+        
+        each entry is a regex and any path that matches it will be ignored 
+        by the git-svn bridge.
+
+        SA git config key: svn-remote.svn.ignore-paths
+        """
+        return self._ignore_paths
+
+    def __init__(self, url, branches, ignore_paths):
         self._url = url
         self._branches = set(branches)
+        self._ignore_paths = set(ignore_paths)
 
     @classmethod
     def parseConfig(cls, config:str):
@@ -216,22 +256,33 @@ class GitSvnDef(object):
         yamlConfig = yaml.load(config)
 
         # check config data
+        # 1) url
         if 'url' not in yamlConfig:
             raise Exception("missing key 'url'")
         if type(yamlConfig['url']) is not str:
             raise Exception("expected a url value for the url key. but got" + str(yamlConfig['url']))
+        # 2) branches
         if 'branches' not in yamlConfig:
             raise Exception("missing key 'branches'. At least one svn branch expected")
-        
         if type(yamlConfig['branches']) is not list:
             raise Exception("expected a list for the branches key. but got" + str(yamlConfig['branches']))
         for b in yamlConfig['branches']:
             if type(b) is not str:
                 raise Exception("Each of the branches key list items should be a url string.  but got" + str(yamlConfig['url']))
 
-        # 
-        return GitSvnDef(yamlConfig['url'], yamlConfig['branches'])
+        # 3) ignore-paths
+        if 'ignore-paths' not in yamlConfig:
+            return GitSvnDef(yamlConfig['url'], yamlConfig['branches'], [])
 
+        if type(yamlConfig['ignore-paths']) is not list:
+            raise Exception("expected a list of ignore-paths, but got " + str(yamlConfig['ignore-paths']))
+        
+        for p in yamlConfig['ignore-paths']:
+            if type(p) is not str:\
+                raise Exception("Each of the ignore-paths key list items should be a str, but got " + str(p))
+        
+
+        return GitSvnDef(yamlConfig['url'], yamlConfig['branches'], yamlConfig['ignore-paths'])
 
     
 
