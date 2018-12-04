fbbdfd589bb48134bfeceb30a41dc1b4eea403d1
JDVlieger <jeroen.devlieger@nikon.com>
2018-12-04 12:29:27 +0100

    #4 don't set ignore-paths git config key if not neccessary

--- git_svn/gitSvnDefInit.py
+++ git_svn/gitSvnDefInit.py
@@ -168,6 +168,10 @@ def add_git_svn_ignore_paths(ignore_paths:list):
     """add the ignore_paths to the git-svn bridge config
     """
 
+    # nothing to do is nothing is ignored
+    if len(ignore_paths) == 0:
+        return
+
     # fail if the config key is already set
     cli = ["git", "config", "--local", "--get", "svn-remote.svn.ignore-paths"]
     rc = subprocess.call(cli)
